de371a1639cf2c197150013bac15819842654d9a
IsmailYaman <iso_yaman@outlook.com>
2023-12-14 13:56:33 +0100

    Added go back function of the nextjs router

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,15 @@
+'use client'
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
 import Link from "next/link";
 import React from "react";
 
 const Header = ({ title }: { title: string }) => {
+    const router = useRouter()
     return (
         <div className="navbar bg-primary">
             <div className="flex-1">
-                <Link href="/" className="btn btn-ghost text-white">
+                <Link href="#" onClick={() => router.back()} className="btn btn-ghost text-white">
                     <Image src={"/img/back.svg"} width={20} height={20} alt="logo" />
                     {title}
                 </Link>
--- app/ehbo/page.tsx
+++ app/ehbo/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../components/navbar";
 import Header from "../components/header";
@@ -8,19 +7,19 @@ const EhboHome = () => {
     return (
         <>
             <Header title="Eerste hulp bij hartaanvallen"/>
-            <div className="container mx-auto px-16">
-            <div className="flex flex-col h-screen justify-center items-stretch">
-                <Link className="btn btn-secondary mb-5" href="ehbo/handelingen">
-                    Handelingen tijdens eerste hulp
-                </Link>
-                <Link className="btn btn-secondary mb-5" href="ehbo/oorzaken">
-                    Wat zijn de oorzaken van een hartaanval?
-                </Link>
-                <Link className="btn btn-secondary" href="ehbo/symptomen">
-                    Wat zijn de symptomen van een hartaanval?
-                </Link>
+            <div className="flex justify-center items-center h-screen">
+                <div className="flex flex-col">
+                    <Link className="btn btn-secondary mb-5" href="ehbo/handelingen">
+                        Handelingen tijdens eerste hulp
+                    </Link>
+                    <Link className="btn btn-secondary mb-5" href="ehbo/oorzaken">
+                        Wat zijn de oorzaken van een hartaanval?
+                    </Link>
+                    <Link className="btn btn-secondary" href="ehbo/symptomen">
+                        Wat zijn de symptomen van een hartaanval?
+                    </Link>
+                </div>
             </div>
-        </div>
             <Navbar />
         </>
     );
