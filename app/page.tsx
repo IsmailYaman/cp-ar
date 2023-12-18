9b2b35f966940fdacd8cb1695aeab5094e1069d4
IsmailYaman <iso_yaman@outlook.com>
2023-12-18 13:02:47 +0100

    Final final finishing touches

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="container mx-auto px-16">
             <div className="flex flex-col h-screen justify-center items-stretch">
-                <div className="mask mask-squircle bg-gradient-to-b from-primary to-red-700 w-24 h-24 mx-auto p-3">
+                <div className="mask mask-squircle bg-gradient-to-b from-primary to-red-700 w-30 h-30 mx-auto p-3">
                     <Image src="img/logo.svg" width={100} height={100} alt="logo" />
                 </div>
                 <h1 className="text-center pb-10 pt-5">CP-AR</h1>
