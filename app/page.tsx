01fdc7d8a76784e2e8969c12ca222423040ceb3c
IsmailYaman <iso_yaman@outlook.com>
2023-12-18 15:27:34 +0100

    Final final final finishing touches

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,23 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="container mx-auto px-16">
-            <div className="flex flex-col h-screen justify-center items-stretch">
-                <div className="mask mask-squircle bg-gradient-to-b from-primary to-red-700 w-30 h-30 mx-auto p-3">
-                    <Image src="img/logo.svg" width={100} height={100} alt="logo" />
+        <div className="flex flex-col h-screen">
+            <div className="flex justify-center flex-grow items-center">
+                <div className="flex flex-col w-96">
+                    <div className="mask mask-squircle bg-gradient-to-b from-primary to-red-700 w-30 h-30 mx-auto p-3">
+                        <Image src="img/logo.svg" width={100} height={100} alt="logo" />
+                    </div>
+                    <h1 className="text-center pb-10 pt-5">CP-AR</h1>
+                    <Link className="btn btn-secondary mb-5" href="/ehbo">
+                        Eerste hulp bij hartaanvallen
+                    </Link>
+                    <Link className="btn btn-secondary mb-5" href="/aed">
+                        AED training
+                    </Link>
+                    <Link className="btn btn-primary" href="https://360fabriek.8thwall.app/ismail-test/">
+                        AR scenario&#39;s
+                    </Link>
                 </div>
-                <h1 className="text-center pb-10 pt-5">CP-AR</h1>
-                <Link className="btn btn-secondary mb-2" href="/ehbo">
-                    Eerste hulp bij hartaanvallen
-                </Link>
-                <Link className="btn btn-secondary mb-2" href="/aed">
-                    AED training
-                </Link>
-                <Link className="btn btn-primary" href="https://360fabriek.8thwall.app/ismail-test/">
-                    AR scenario&#39;s
-                </Link>
             </div>
         </div>
     );
