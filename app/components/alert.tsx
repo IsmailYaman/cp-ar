f1cf0e162b257aa9964d7f581e58d4199ab12c69
Ismail <iso_yaman@outlook.com>
2024-02-01 21:14:42 +0100

    Fixed nextjs warning about using sessionStorage

--- app/components/alert.tsx
+++ app/components/alert.tsx
@@ -3,16 +3,26 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Alert = () => {
-    const [isVisible, setIsVisible] = useState(() => {
-        const isAlertClosed = sessionStorage.getItem("isAlertClosed");
-        return isAlertClosed ? false : true;
-    });
+    const [isVisible, setIsVisible] = useState(true);
 
+    useEffect(() => {
+        // Check sessionStorage to see if the alert has been closed already in this session
+        const isAlertClosed =
+            sessionStorage.getItem("isAlertClosed") === "true";
+        // If isAlertClosed is true, set isVisible to false
+        if (isAlertClosed) {
+            setIsVisible(false);
+        }
+    }, []); // The empty array ensures this effect runs only once after initial render
+
+    // Function to hide the alert and set the state in sessionStorage
     const handleClose = () => {
         setIsVisible(false);
+        // Save the state to sessionStorage when the user closes the alert
         sessionStorage.setItem("isAlertClosed", "true");
     };
 
+    // Only render the alert if isVisible is true
     if (!isVisible) return null;
 
     return (
@@ -36,11 +46,13 @@ const Alert = () => {
                     onClick={handleClose}
                     className="btn btn-sm btn-primary"
                 >
+                    {/* Ensure the Image component uses the correct `layout` prop for Next.js */}
                     <Image
                         src="/img/close.svg"
+                        alt="Close"
                         width={20}
                         height={20}
-                        alt="Close"
+                        layout="fixed"
                     />
                 </button>
             </div>
